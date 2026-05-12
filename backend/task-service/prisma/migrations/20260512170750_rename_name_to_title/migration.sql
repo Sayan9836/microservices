cde70a8e350ed74bcdab75e06e8a19a7acd4074e
Sayan9836 <sayanearnings209@gmail.com>
2026-05-12 22:45:50 +0530

    rename name field to title

---
+++ backend/task-service/prisma/migrations/20260512170750_rename_name_to_title/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `todos` table. All the data in the column will be lost.
+  - Added the required column `title` to the `todos` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "todos" RENAME COLUMN "name" TO "title";
