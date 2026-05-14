ac3625d732f9b430aac1fde3f1aa1b76ad15139f
Sayan9836 <sayanearnings209@gmail.com>
2026-05-14 23:49:16 +0530

    migration and added tags table

--- backend/task-service/prisma/migrations/0_init/migration.sql
+++ backend/task-service/prisma/migrations/0_init/migration.sql
@@ -9,10 +9,25 @@ CREATE TABLE "todos" (
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "userId" TEXT NOT NULL,
+    "categoryId" TEXT,
 
     CONSTRAINT "todos_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE INDEX "todos_userId_idx" ON "todos"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+
+-- AddForeignKey
+ALTER TABLE "todos" ADD CONSTRAINT "todos_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
